5ee360b4c9ab69ea34ce22c1494140159a88ce3d
jupiterorbita <38020888+jupiterorbita@users.noreply.github.com>
2021-09-24 11:57:08 -0700

    prepare w1d2 algos

---
+++ Algos/W1D2_BST_insert/w1d2.js
@@ -0,0 +1,41 @@
+class Node{
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class SLL{
+    constructor(){
+        this.head = null;
+    }
+
+    isEmpty(){
+        if (this.head) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    // when a pointer is to the LEFT of an equal sign, we are CHANGING it
+    // when a pointer is to the RIGHT of an equal sign, we are READING it
+
+    // add given node to the head, if it exists. return void
+    // list is empty?
+    // list already has nodes?
+    addToFront(node) { }
+
+    // create a new node with given data, add it to the head. return void
+    addDataToFront(data) {}
+}
+
+// instantiate the SLL
+var myCoolList = new SLL();
+
+// creating nodes
+var myCoolNode = new Node(11);
+
+// executing methods:
+myCoolList.addToFront(myCoolNode);
+// myCoolList.addToFront(new Node(11));
